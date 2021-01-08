bf77b5fe05071b2e3558f1697a9f4da9a32ded35
Javier De La Fuente <javierdelafuentejr@gmail.com>
2021-01-07 20:48:35 -0600

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -67,14 +67,14 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  let fileName = `${exports.dataDir}/${id}.txt`;
+  fs.unlink(fileName, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
